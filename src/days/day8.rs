3b5dc8e4c8147c3cbe2e99360b5993bcfd416d1d
AndrejKlim <andrei.klimovich98@mail.ru>
2022-12-09 10:43:00 +0300

    !!NOT FINISHED!!! day8 part 2

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -8,39 +8,63 @@ pub fn get_visible_trees() {
         .collect::<Vec<Vec<u32>>>();
 
     let mut visible_trees_count = 0;
+    let mut max_scenic_score = 0;
 
     for i in 1..grid.len()-1 {
        '_inner: for j in 1..grid.get(0).unwrap().len()-1 {
            let tree = grid.get(i).unwrap().get(j).unwrap();
 
-           let to_left_max = grid.get(i).unwrap()[..j].iter().max().unwrap();
-           if tree > to_left_max {
-               visible_trees_count += 1;
-               continue '_inner;
-           }
-           let to_right_max = grid.get(i).unwrap()[j + 1..].iter().max().unwrap();
-           if tree > to_right_max {
-               visible_trees_count += 1;
-               continue '_inner;
-           }
+
+           let to_left_max = grid.get(i).unwrap()[..j].iter().enumerate().max_by_key(|(_idx, &e)| e).unwrap();
+
+           let to_right_max = grid.get(i).unwrap()[j + 1..].iter().enumerate().max_by_key(|(_idx, &e)| e).unwrap();
+
            let to_up_max = grid.iter()
                .map(|vec| vec.get(j).unwrap())
                .take(i)
-               .max().unwrap();
-           if tree > to_up_max {
-               visible_trees_count += 1;
-               continue '_inner;
-           }
+               .enumerate()
+               .max_by_key(|(_idx, &e)| e).unwrap();
+
            let to_down_max = grid.iter()
                .map(|vec| vec.get(j).unwrap())
                .skip(i + 1)
-               .max().unwrap();
-           if tree > to_down_max {
-               visible_trees_count += 1;
+               .enumerate()
+               .max_by_key(|(_idx, &e)| e).unwrap();
+
+           let mut scenic_score = 1;
+           if tree > to_left_max.1 {
+               scenic_score *= j;
+           } else {
+               scenic_score *= j - to_left_max.0;
            }
+
+           if tree > to_right_max.1 {
+               scenic_score *= grid.get(0).unwrap().len() - 1 - j;
+           } else {
+               scenic_score *= to_right_max.0 - j;
+           }
+
+           if tree > to_up_max.1 {
+               scenic_score *= i;
+           } else {
+               scenic_score *= i - to_up_max.0;
+           }
+
+           if tree > to_down_max.1 {
+               scenic_score *= grid.len() - 1 - i;
+           } else {
+               scenic_score *= to_down_max.0 - i;
+           }
+
+           if scenic_score > max_scenic_score {
+               max_scenic_score = scenic_score;
+           }
+
+           println!()
        }
     }
 
     println!("Visible trees {}", visible_trees_count + ((grid.len()-1) * 4));
+    println!("max scenic score {}", max_scenic_score );
 
 }
